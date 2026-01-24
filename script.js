45fc5f1048a385ae87d18b4f402ea5b79a84e204
Jatofi <56402844+jatofi-official@users.noreply.github.com>
2026-01-24 12:38:12 +0100

    Added fleur pattern

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const mcPalette = [
 // List your filenames here (ensure these exist in patterns/vanilla/ as .png)
 const patterns = ['none', "base","border","bricks","circle","creeper","cross","curly_border","diagonal_left","diagonal_right","diagonal_up_left","diagonal_up_right","flower","flow","globe","gradient","gradient_up","guster","half_horizontal_bottom","half_horizontal","half_vertical","half_vertical_right","mojang","piglin","rhombus","skull","small_stripes","square_bottom_left","square_bottom_right","square_top_left","square_top_right","straight_cross","stripe_bottom","stripe_center","stripe_downleft","stripe_downright","stripe_left","stripe_middle","stripe_right","stripe_top","triangle_bottom","triangles_bottom","triangles_top","triangle_top"];
 
-const extraPatterns = ["anchor","blam","castle","chequered","circle_tiles","clubs","cogs","companion","crown","curtains","diamonds","double_bars","double_gradient","emoji","eye","fancy","ghast","hammer","hearts","horn","knot","moon","palace","peace","pillager","pumpkin","pyramid","revolution","ribs","shield","spades","sun","sword","tattered","tower","trident","villager","yin_yang"];
+const extraPatterns = ["anchor","blam","castle","chequered","circle_tiles","clubs","cogs","companion","crown","curtains","diamonds","double_bars","double_gradient","emoji","eye","fancy","ghast","hammer","hearts","horn","knot","moon","palace","peace","pillager","pumpkin","pyramid","revolution","ribs","shield","spades","sun","sword","tattered","tower","trident","villager","yin_yang","fleur"];
 
 // State management
 let currentPatternSet = patterns; 
